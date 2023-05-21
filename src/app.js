bf99e94e6ad50bd8bb2c14fe726370f1640ca130
Alencaminha <alexandrencaminha@gmail.com>
2023-05-21 19:15:15 -0300

    Added nodemon and sqlite to the project

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
-app.get("/", function(req, res) {
+app.get("/", (req, res) => {
     res.send("Teste");
+});
+
+app.post("/pessoa", (req, res) => {
+    console.log(req.body);
+    res.json({
+        "statusCode": 200
+    });
 });
 
 app.listen(3000, () => {
