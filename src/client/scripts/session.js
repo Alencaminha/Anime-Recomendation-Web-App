d5e002cda77672eaa3f4bc39fea42b60538ed1d3
KaMatiolli <karinamatiolli@gmail.com>
2023-06-14 22:31:20 -0300

    mudança para port. e conf. do nav-bar

--- src/client/scripts/session.js
+++ src/client/scripts/session.js
@@ -1,5 +1,20 @@
-/*window.onload = () => {
-    if (sessionStorage.getItem("loggedUserId") == null) {
-        window.location.href = "login.html";
-    }
-}*/
+window.onload = () => {
+    fetch("http://localhost:3000/readuser", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            id: sessionStorage.getItem("loggedUserId")
+        })
+    })
+
+    .then(res => res.json())
+    .then(userData => {
+        document.getElementById("usernameIdentification").innerHTML = "Olá, " + userData.username;
+        // For user input box
+        fetch(`https://api.jikan.moe/v4/anime/${userData.input_id}`)
+        .then(res => res.json())
+        .then(data => {document.getElementById("inputtedAnime").value = data.data.title_english});
+    });
+}
