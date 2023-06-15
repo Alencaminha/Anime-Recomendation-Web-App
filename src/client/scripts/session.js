73b53c7aaf486bb639714b5b18074b62ed34a0cd
KaMatiolli <karinamatiolli@gmail.com>
2023-06-15 15:36:28 -0300

    Conexão user em todos navbar

--- src/client/scripts/session.js
+++ src/client/scripts/session.js
@@ -7,3 +7,33 @@ window.onload = () => {
 quit = () => {
     sessionStorage.setItem("loggedUserId", null);
 }
+window.onload = () => {
+    fetch("http://localhost:3000/readuser", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            id: sessionStorage.getItem("loggedUserId")
+        })
+    })
+    .then(res => res.json())
+    .then(userData => {
+        document.getElementById("usernameIdentification").innerHTML = "Olá, " + userData.username;
+        // For user input box
+    });
+}
+
+updateUser = (fieldToChange, newValue) => {
+    fetch("http://localhost:3000/updateuser", {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                field: fieldToChange,
+                value: newValue,
+                id: sessionStorage.getItem("loggedUserId")
+            })
+        });
+}
